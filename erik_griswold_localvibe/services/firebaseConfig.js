b3ae09e5d136f61a651807aeb1d87bee28d5a3ea
EriG15 <120547715+EriG15@users.noreply.github.com>
2024-04-18 23:06:22 -0700

    Add files via upload
    firebase support added kind of

---
+++ erik_griswold_localvibe/services/firebaseConfig.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase'
+
+  const firebaseConfig = {
+  apiKey: "AIzaSyDflZqKU3TIN6iaqYaAB8BZtr9nz16y_dI",
+  authDomain: "localvibe-84968.firebaseapp.com",
+  projectId: "localvibe-84968",
+  storageBucket: "localvibe-84968.appspot.com",
+  messagingSenderId: "125966739424",
+  appId: "1:125966739424:web:5ba20b4ce39c381cdfed85"
+};
+
+let app = !firebase.default.apps.length ? firebase.default.initializeApp(firebaseConfig) : firebase.default.app();
+const auth = firebase.default.auth(app);
+const db = app.firestore();
+
+
+export { db, app, auth };
+
